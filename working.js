fedcedd22ade4a96fed3cf0f74cd28c62b220ef8
Ashikur-CSE <ashikur201.cse@gmail.com>
2022-10-13 23:05:07 +0600

    Almost Done

--- working.js
+++ working.js
@@ -28,27 +28,51 @@ minusButton2.addEventListener("click",function () {
 
 //Main Function
 function plusMinusCart(product,isIncrease){
-    const cartField1=document.getElementById(product + "-count").value;
-    const cartField1Num=parseInt(cartField1);
-    let caseNewCount=cartField1Num;
+    const productQuantity=document.getElementById(product + "-count").value;
+    const productQuantityNum=parseInt(productQuantity);
+    let productNewCount=productQuantityNum;
 
     if (isIncrease==true){
-        caseNewCount=cartField1Num + 1;
+        productNewCount=productQuantityNum + 1;
         
     }
-    if (isIncrease==false && caseNewCount>1){
-        caseNewCount=cartField1Num - 1;
+    if (isIncrease==false && productNewCount>1){
+        productNewCount=productQuantityNum - 1;
     }
     let totalPrice=0;
     if (product=="phone"){
-        totalPrice=caseNewCount*1250;
+        totalPrice=productNewCount*1250;
     }
     if (product=="case"){
-        totalPrice=caseNewCount*59;
+        totalPrice=productNewCount*59;
     }
-    document.getElementById(product + "-count").value=caseNewCount;
+    document.getElementById(product + "-count").value=productNewCount;
     document.getElementById(product + "-price").innerText=totalPrice;
+
+    calculationTotal();
 };
+
+//Grand Calculation
+function calculationTotal(){
+    const phoneQuantity=getProductQuantity("phone");
+    const caseQuantity=getProductQuantity("case");
+
+    const totalAmount=phoneQuantity*1250 + caseQuantity*59;
+    document.getElementById("subTotalPrice").innerText=totalAmount;
+
+    const tax=(totalAmount*0.1).toFixed(2);
+    document.getElementById("tex").innerText=tax;
+
+    const grandTotal=parseInt(totalAmount)+parseFloat(tax);
+    document.getElementById("GrandtotalPrice").innerText=grandTotal;
+}
+
+function getProductQuantity(product){
+    const productInput= document.getElementById(product + "-count").value;
+    const productCount=parseInt(productInput);
+    return productCount;
+
+}
 
 
 
