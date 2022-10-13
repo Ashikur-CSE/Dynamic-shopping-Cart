42e2c678b24f18258957ec5f70758011228d0766
Ashikur-CSE <ashikur201.cse@gmail.com>
2022-10-13 22:26:04 +0600

    Cart work done

--- working.js
+++ working.js
@@ -1,75 +1,54 @@
-// 1st cart:
+// Phone cart:
 // For plus button
 const plusButton1 = document.getElementById("plus-button1");
 plusButton1.addEventListener("click",function () {
-    var totalQuantity=plusMinusCart();
-    const totalCartField=totalQuantity+1;
-    document.getElementById("cart-field1").value=totalCartField;
-
-    const totalPrice=totalCartField*59;
-    document.getElementById("price1").innerText=totalPrice;
-    
+    plusMinusCart("phone",true);  
 });
 
 //For minus button
 const minusButton1=document.getElementById("minus-button1");
 minusButton1.addEventListener("click",function () {
-    var totalQuantity=plusMinusCart();
-    const totalCartField=totalQuantity-1;
-    document.getElementById("cart-field1").value=totalCartField;
-    const totalPrice=totalCartField*59;
-    document.getElementById("price1").innerText=totalPrice;
-
-    if (totalCartField<1){
-        document.getElementById("cart-field1").value=1;
-        document.getElementById("price1").innerText=59;
-
-    }
-
-    
+    plusMinusCart("phone",false); 
 });
 
-//2nd Cart:
+//Case Cart:
 // For plus button
 const plusButton2 = document.getElementById("plus-button2");
 plusButton2.addEventListener("click",function () {
-    var totalQuantity2=plusMinusCart2();
-    const totalCartField2=totalQuantity2+1;
-    document.getElementById("cart-field2").value=totalCartField2;
 
-    const totalPrice2=totalCartField2*180;
-    document.getElementById("price2").innerText=totalPrice2;
-   
-    
+    plusMinusCart("case",true);
 });
 
 //For minus button
 const minusButton2=document.getElementById("minus-button2");
 minusButton2.addEventListener("click",function () {
-    var totalQuantity2=plusMinusCart2();
-    const totalCartField2=totalQuantity2-1;
-    document.getElementById("cart-field2").value=totalCartField2;
-    const totalPrice2=totalCartField2*180;
-    document.getElementById("price2").innerText=totalPrice2;
-    if (totalCartField2<1){
-        document.getElementById("cart-field2").value=1;
-        document.getElementById("price2").innerText=180;
-    }
-
-    
+    plusMinusCart("case",false);
 });
 
 
-function plusMinusCart(){
-    const cartField1=document.getElementById("cart-field1").value;
+//Main Function
+function plusMinusCart(product,isIncrease){
+    const cartField1=document.getElementById(product + "-count").value;
     const cartField1Num=parseInt(cartField1);
-    return cartField1Num;
+    let caseNewCount=cartField1Num;
 
+    if (isIncrease==true){
+        caseNewCount=cartField1Num + 1;
+        
+    }
+    if (isIncrease==false && caseNewCount>1){
+        caseNewCount=cartField1Num - 1;
+    }
+    let totalPrice=0;
+    if (product=="phone"){
+        totalPrice=caseNewCount*1250;
+    }
+    if (product=="case"){
+        totalPrice=caseNewCount*59;
+    }
+    document.getElementById(product + "-count").value=caseNewCount;
+    document.getElementById(product + "-price").innerText=totalPrice;
 };
 
-function plusMinusCart2(){
-    const cartField2=document.getElementById("cart-field2").value;
-    const cartField2Num=parseInt(cartField2);
-    return cartField2Num;
 
-};
+
